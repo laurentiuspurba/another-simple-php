160e42d9b683aadcda70181db2cfa993dfc8813f
laurentiuspurba <lrpurba@yahoo.com>
2017-11-13 18:24:29 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset=utf-8 />
+<title>JS Bin</title>
+  <script type="text/javascript">
+    var index=0;
+    function changeBanner(){ 
+      [].forEach.call(document.images,function (v,i) { document.images[i].hidden = i!==index});
+      index = (index+1) % document.images.length;
+    }
+    window.onload = function () {setInterval(changeBanner, 1000)};
+  </script>
+</head>
+<body>
+  <div id="wrapper">
+    <div>
+        <img src="images/1.jpeg" />
+      <img src="images/2.jpeg" />
+      <img src="iamges/3.jpeg" />
+    </div>
+  </div>
+</body>
+</html>
+
 <?php
 
   echo '<br/>';
   echo '<strong><h1>Another page!!!</h1></strong>';
   echo '<br/>';
 
+  /*
   $dir = './images/*.jpeg';
   $images = glob($dir);
 
@@ -11,3 +37,4 @@
     echo "<img src='" . $image . "' />";
     sleep(8);
   }
+  */
